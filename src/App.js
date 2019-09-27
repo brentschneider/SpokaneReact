da358291fbf3cf0209ed2f90fa1eceb2fd9668ba
Brent Schneider <brent@milkshakeinteractive.com>
2019-09-26 23:46:27 -0700

    SPA part two and week two assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component} from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 // Styling
-import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 // Components
-import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
 
@@ -20,9 +19,11 @@ class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
