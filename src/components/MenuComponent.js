9a6e6664b86444ccfed458dddf478c00be4149fa
Brent Schneider <brent@milkshakeinteractive.com>
2019-10-11 23:29:39 -0700

    Fetch Post Comment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,12 +3,13 @@ import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderMenuItem({ dish, onClick }) {
-    return(
+    return (
         <Card>
             <Link to={`/menu/${dish.id}`} >
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
@@ -63,7 +64,7 @@ const Menu = (props) => {
                     {menu}
                 </div>
             </div>
-        );
+    );
 };
 
 export default Menu;
