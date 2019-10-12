9be54520afde4b14ed9d6014b3c277cf38f67e02
Brent Schneider <brent@milkshakeinteractive.com>
2019-10-11 22:41:44 -0700

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,8 @@ import { Card, CardImg, CardText, CardBody,
     Modal, ModalHeader, ModalBody, Button, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
+    
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 
@@ -20,16 +21,16 @@ function RenderDish({dish}) {
             </Card>
         </div>
     );
-}
+}  
 
 function RenderComments({comments, addComment, dishId}) {
     if (comments != null) 
-        return (
+        return(
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
                     {comments.map((comment) => {
-                        return(
+                        return (
                             <li key={comment.id}>
                                 <p>{comment.comment}</p>
                                 <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', 
@@ -41,32 +42,32 @@ function RenderComments({comments, addComment, dishId}) {
                 </ul>
                 <CommentForm dishId={dishId} addComment={addComment} />
             </div>
-        );
+        );    
     else
-        return (
+        return(
             <div></div>
-        );
+        );       
 }
-
+    
 class CommentForm extends Component {
+
     constructor(props) {
         super(props);
 
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    
+
         this.state = {
             isModalOpen: false          
         };
-
     }
-    
+
     toggleModal() {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
-    }    
-  
+    }
+
     handleSubmit(values) {
         this.toggleModal();
         this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
@@ -75,35 +76,35 @@ class CommentForm extends Component {
     render() {
         return(
             <div>
-                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>  
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
                         <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                            <Row className="form-group">
+                            <Row className="form-group">               
                                 <Col>
                                     <Label htmlFor="rating">Rating</Label>
                                     <Control.select model=".rating" id="rating" name="rating"
-                                        className="form-control">
+                                            className="form-control">
                                         <option>1</option>
-                                        <option>2</option>                                       
+                                        <option>2</option>                                    
                                         <option>3</option>
                                         <option>4</option>
-                                        <option>5</option>
+                                        <option>5</option>                                                                                                            
                                     </Control.select>
                                 </Col>
-                            </Row>            
+                            </Row>
                             <Row className="form-group">
                                 <Col>
-                                <Label htmlFor="author" >Your Name</Label>
-                                    <Control.text model=".author" id="author" name="author"
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author"
                                         placeholder="Your Name" 
                                         className="form-control"
                                         validators={{
                                             minLength: minLength(3), 
                                             maxLength: maxLength(15)
                                         }}
-                                         />
+                                    />
                                     <Errors
                                         className="text-danger"
                                         model=".author"
@@ -112,15 +113,15 @@ class CommentForm extends Component {
                                             minLength: 'Must be greater than 2 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
-                                     />                                        
-                                </Col>
+                                     />
+                                </Col>        
                             </Row>
                             <Row className="form-group">
                                 <Col>
                                 <Label htmlFor="comment">Comment</Label>
-                                    <Control.textarea model=".comment" id="comment" name="comment"
-                                        rows="6"
-                                        className="form-control" />
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    rows="6"
+                                    className="form-control" />
                                 </Col>
                             </Row>    
                             <Button type="submit" className="bg-primary">
@@ -135,7 +136,25 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null )
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)     
         return (
             <div className="container">
                 <div className="row">
@@ -150,8 +169,8 @@ const DishDetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} 
-                        addComment={props.addComment} 
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
                         dishId={props.dish.id}
                     />
                 </div>
@@ -160,7 +179,7 @@ const DishDetail = (props) => {
     else
         return(
             <div></div>
-        );
-}
+        );            
+}    
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,31 +2,51 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
+function RenderMenuItem({ dish, onClick }) {
+    return(
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
+    );
+}
 
-    function RenderMenuItem ({dish, onClick}) {
+const Menu = (props) => {
+    const menu = props.dishes.dishes.map((dish) => {
         return (
-             <Card>
-                <Link to={`/menu/${dish.id}`} >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Link>
-            </Card>
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenuItem dish={dish} />
+            </div>
+        );
+    });
+
+    if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
         );
     }
-
-    const Menu = (props) => {
-
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} />
+    else if (props.dishes.errMess) {
+        return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
                 </div>
-            );
-        });
-
+            </div>
+        );
+    }
+    else
         return (
             <div className="container">
                 <div className="row">
@@ -44,6 +64,6 @@ import { Link } from 'react-router-dom';
                 </div>
             </div>
         );
-    }
+};
 
 export default Menu;
